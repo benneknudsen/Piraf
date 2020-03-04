cc748ada0e43de9e68ef679d7063a4cc54a9fe10
Benjamin Knudsen <benjaschou12@yahoo.dk>
2020-03-04 14:10:08 +0100

    Mangler kun navbar til mobil

--- main.js
+++ main.js
@@ -60,13 +60,21 @@
   });
 
 
-  $(".burger").click(function() {
-    $(this).toggleClass('active');
-    $("ul.menu li").slideToggle('fast');
-})
-
-$(window).resize(function() {
-    if ($(window).width() > 650) {
-        $('ul.menu li').removeAttr('style');
-    }
-})
+  (function($) {
+    $(function() {
+      $('nav ul li > a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+      $('#nav-toggle').click(function() {
+    $('nav ul').toggle();
+  });
+    });
+  })(jQuery);
