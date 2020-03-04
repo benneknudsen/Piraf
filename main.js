6a109c00d57f01e82a9c35aa61ece73503260c72
Benjamin Knudsen <benjaschou12@yahoo.dk>
2020-03-04 15:17:43 +0100

    Mangler kun drawer design

--- main.js
+++ main.js
@@ -60,21 +60,78 @@
   });
 
 
-  (function($) {
-    $(function() {
-      $('nav ul li > a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
-      });
-      $('html').click(function() {
-        $('.nav-dropdown').hide();
-      });
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-      $('#nav-toggle').click(function() {
-    $('nav ul').toggle();
-  });
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+  
+      }
     });
-  })(jQuery);
+  }
+  
+  
+  
+  
+  
+  $(document).ready(function () {
+    $('#drawer-toggle').click(function () {
+      $('#nav-icon1').toggleClass('open');
+    });
+  });
+  
+  
+  $(document).ready(function () {
+    $('#drawer-togglem').click(function () {
+      $('#nav-icon1m').toggleClass('open');
+    });
+  });
+  
+  
+  var dropdown1 = document.getElementsByClassName("dropdown-btn-m");
+  var i;
+  
+  for (i = 0; i < dropdown1.length; i++) {
+    dropdown1[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+  
+  
+  $(".rotate").click(function () {
+    $(this).toggleClass("down");
+  })
+  
+  
+  $(document).ready(function () {
+    $('.dropdown-btn-m').click(function () {
+      $('.dropdown-container').toggleClass('up');
+    });
+  });
+  
+  
+  
+  $(document).ready(function () {
+    $('.arrow').click(function () {
+      $('.roter').toggleClass('ned');
+    });
+  });
+  
+  $(document).ready(function () {
+    $('.arrow1').click(function () {
+      $('.roter1').toggleClass('ned');
+    });
+  });
+  
